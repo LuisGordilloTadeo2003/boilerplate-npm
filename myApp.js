b3fdc317f2c9f49b9842d3f00f0cd2f51d0bc81e
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-09 19:25:15 +0000

    punto4.1

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const path = require('path');
+
+const app = express();
 
 const publicPath = path.join(__dirname, 'public');
 app.use('/public', express.static(publicPath));
 
 app.get('/', (req, res) => {
-    absolutePath = __dirname + '/views/index.html'
-    res.sendFile(absolutePath)
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 module.exports = app;
