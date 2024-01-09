3bc78ce6fb550e0e1d0cba8015e67cc57a730df8
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-09 21:14:54 +0000

    punto8.2

--- myApp.js
+++ myApp.js
@@ -23,14 +23,18 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/now', (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-},
+app.get(
+    "/now",
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
     (req, res) => {
-        res.json({ time: req.time });
+        res.send({
+            time: req.time
+        });
     }
-)
+);
 
 
 
